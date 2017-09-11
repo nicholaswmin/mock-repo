ae2643083904f8298069a2eabcb2844851ff9fd6
Nicholas Kyriakides <nicholaswmin@Nicholass-iMac.local>
2017-09-11 23:20:17 +0300

    restructured tests

---
+++ test/usage.spec.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const chai = require('chai')
+const MockRepo = require('../index.js')
+
+const expect = chai.expect
+chai.should()
+
+let userRepo
+
+beforeEach(() => {
+  userRepo = new MockRepo({
+    primary_key: 'id_user'
+  })
+})
+
+describe('README usage instructions', () => {
+  it('does what is says on the label', () => {
+    return userRepo.upsert(null, { id_user: 1, name: 'John Doe' })
+      .then(() => {
+        return userRepo.upsert(null, { id_user: 1, name: 'John Doooe' })
+      })
+      .then(() => {
+        return userRepo.get(null, { id_user: 1 })
+      })
+      .then(result => {
+        result.should.deep.equal({ id_user: 1, name: 'John Doooe' })
+        return
+      })
+      .then(() => {
+        userRepo.items.length.should.equal(1)
+      })
+  })
+})
